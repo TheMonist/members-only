ac318754270ca579a3ee8b6be955eadcaa3b21c0
TheMonist <rogerrosena@gmail.com>
2024-12-02 22:34:33 -0500

    installing express to js app

---
+++ app.js
@@ -0,0 +1,10 @@
+// this is just to test if everything is working
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => {
+    console.log(`Express App on PORT ${PORT}`);
+})

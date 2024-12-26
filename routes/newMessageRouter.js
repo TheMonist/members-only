07a15af48bdd5c4f7d2f8fcc91f2c50e797a5ed3
TheMonist <rogerrosena@gmail.com>
2024-12-26 13:01:12 -0500

    working on router files

---
+++ routes/newMessageRouter.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const newMessageRouter = Router();
+const db = require("../db/queries");
+
+newMessageRouter.get("/:id", (req, res) => {
+    res.redirect("/");
+});
+
+newMessageRouter.post("/:id", async (req, res) => {
+    const messageId = req.params.id;
+    console.log(messageId);
+
+    try {
+        await db.deleteMessage(messageId);
+        res.redirect("/")
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+        res.json({ succees: false, error: "Failed To Delete Message" });
+    }
+});
+
+module.exports = newMessageRouter;
---
+++ routes/signinRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const signInRouter = Router();
+const signInController = require("../controllers/signUpController");
+
+signInRouter.get("/", signInController.signUpGet);
+signInRouter.post("/", signInController.validateSignUp, signInController.signUpPost);
+
+module.exports = signinRouter;
